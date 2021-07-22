b0e61a5ec4677a66162eb5f10de3b33a42f07552
bharat <bspurohit360@gmail.com>
2021-07-22 21:43:46 +0530

    Update Code + Added GIf

--- Face_Recognition.py
+++ Face_Recognition.py
@@ -1,18 +1,22 @@
+# Import Libraries
 import cv2
 import face_recognition
 import matplotlib.pyplot as plt
 import os ,numpy ,argparse
 from datetime import datetime
 
-
+# Take input Video from terminal(Option)
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-i','--input',default="./Indian_Team1.mp4",type=str,help="Path to Imput video File(Option)")
 arg = vars(parser.parse_args())
 
+# ------- Pre-Process Operations on Input Images  -------
+
 known_img_path =os.path.join( os.getcwd(), 'images')
 # print(os.listdir(known_img_path))
 
+ #--------- Some information about our Candidates in Image --------
 Player_Info = { 'A Kumble':['india','Coach'] ,'Abdul kalam':['india','Scinetist'] ,'B Kumar':['india','Bowler'] ,'Dhawan':['india','Batsman'] ,'Dhoni':['india','Captain'] ,'Elon':['USA','Inventor'] ,'Hardik Panya':['india','All-Rounder'] ,'Jusprit Bumra':['india','Bowler'] ,'KL Rahul':['india','batsman'] ,'M Shami':['india','Bowler'] ,'R Ashwin':['india','Coach'] ,'R Jadeja':['India','All-Rounder'] ,'Rishabh Pant':['india','Batsman'] ,'Rohit Sharma':['india','Batsman'] ,'Virat':['india','Vice-Captain']  ,'U Yadav':['india','Bowler'] , }
 # print(Player_Info)
 
@@ -28,12 +32,14 @@ def Mark_present(Name):
             Speciality = Player_Info[Name][1]
             f.writelines(f'\n{Name},{Country},{Speciality},{now}')        
 
+ # Make the Class of their name 
 All_images = os.listdir(known_img_path)
 Classes=[]
 for i in All_images:
     Classes.append(i.split('.')[0])
 # print(Classes) 
 
+# ------------ Find Encodings of Images ------------
 known_faces_enco=[]
 
 for i in All_images:
@@ -42,6 +48,7 @@ for i in All_images:
     known_faces_enco.append(img_encoding[0])
 
 
+# ------------check if .csv file is present if not then Create One
 file_name ='MyAttandance.csv'
 if file_name not in os.listdir(os.getcwd()):
     open(file_name,'x')
@@ -52,6 +59,7 @@ if file_name not in os.listdir(os.getcwd()):
         Time="Time"
         f.writelines(f'{Name},{Country},{Speciality},{Time}')
 
+# ------------- Take Input to Process  -----------
 input_ = arg['input']
 video  =  cv2.VideoCapture(input_ if(input_) else 0)
 
@@ -63,8 +71,8 @@ while(1):
 #     if(is_cap):
 #         print("video capturing...")
             
-    face_loc = face_recognition.face_locations(frame)
-    face_enco = face_recognition.face_encodings(frame,face_loc) 
+    face_loc = face_recognition.face_locations(frame) # locations of All faces in the fra
+    face_enco = face_recognition.face_encodings(frame,face_loc)  # Encodnings of All faces in the fra
     result = []
     for encode_face, loc_face in zip(face_enco,face_loc):
         
@@ -72,14 +80,14 @@ while(1):
         maches = face_recognition.compare_faces(known_faces_enco, encode_face,tolerance=0.5)
         Dis = face_recognition.face_distance( known_faces_enco, encode_face )
         Person_idx = numpy.argmin(Dis)
-        if( maches[ Person_idx ] ):
+        if( maches[ Person_idx ] ): #if Person Detected
             
             y1,x2,y2,x1 = loc_face
             cv2.rectangle(frame ,(x1,y1),(x2,y2),(0,255,0),2 )
             cv2.rectangle(frame ,(x1,y2-15),(x2,y2),(0,255,0),cv2.FILLED )
             text = f"{Classes[Person_idx]}"
             cv2.putText(frame,text, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (200,0,0),1)
-            Mark_present( Classes[Person_idx] )
+            Mark_present( Classes[Person_idx] ) # mark Present if this is new
 #         print(f"maches {maches}")
 #         print(f"Dis {Dis}")
     
@@ -93,6 +101,7 @@ video.release()
 cv2.destroyAllWindows()
 
 
+# See All saved People in the .csv file Recognised by their Faces
 Entry_names= []
 with open(file_name,'r+') as f:
         Entry_names.append([ i for i in f.read().splitlines()] )
